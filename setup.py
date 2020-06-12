d81530440828fc2c06953f291d25bd14fe8915e1
Natalia Maximo <natalia.maximo@uottawa.ca>
2020-06-11 23:27:54 -0400

    Upped setup.py version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ description = \
 
 setup(
     name='pydeepmerge',
-    version='0.1.1b',
+    version='0.2a',
     license='MIT',
     description=description,
     long_description=long_description,
@@ -16,15 +16,15 @@ setup(
     url='https://github.com/taliamax/pydeepmerge',
     maintainer='Natalia Maximo',
     maintainer_email='iam@natalia.dev',
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     packages=find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )
