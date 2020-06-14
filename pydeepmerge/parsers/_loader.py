5c0aa6c0403f89fec15f244d515d94391a2dc2cb
Natalia Maximo <natalia.maximo@uottawa.ca>
2020-06-14 19:44:26 -0400

    fix: dev: removed f-string dependency for py3.5 compatibility

--- pydeepmerge/parsers/_loader.py
+++ pydeepmerge/parsers/_loader.py
@@ -6,7 +6,7 @@ def load_parsers():
     parsers = {}
 
     for entry_point in pkg_resources.iter_entry_points('pydeepmerge.config_parsers'):
-        logging.debug(f'Found entry_point for {entry_point.name}')
+        logging.debug('Found entry_point for %s', entry_point.name)
         parsers[entry_point.name] = entry_point.load()
 
     return parsers
