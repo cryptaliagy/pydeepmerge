dc280f10cfcb898bc8afa2edcb2498bc59cf341c
andrewnags <anaga042@uottawa.ca>
2020-06-14 20:54:45 -0400

    new: keep_default strategy decorator; when applied to a strategy, if the left value is not found, return the right

--- pydeepmerge/strategies/decorators/_decorators.py
+++ pydeepmerge/strategies/decorators/_decorators.py
@@ -3,6 +3,7 @@ from typing import Mapping
 
 from pydeepmerge import deep_merge
 from pydeepmerge._deep_merge import MergeStrategy
+from pydeepmerge.types import Key
 
 
 def recurse(strategy: MergeStrategy) -> MergeStrategy:
@@ -11,4 +12,13 @@ def recurse(strategy: MergeStrategy) -> MergeStrategy:
         if isinstance(left, Mapping) and isinstance(right, Mapping):
             return deep_merge(left, right, merge_strategy=newstrategy)
         return strategy(left, right)
+    return newstrategy
+
+
+def keep_default(stategy: MergeStrategy) -> MergeStrategy:
+    @ft.wraps(stategy)
+    def newstrategy(left, right):
+        if left is Key.KeyNotFound:
+            return right
+        return stategy(left, right)
     return newstrategy
