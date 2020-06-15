e512b5b386e40becb0d625701453aa732a71150e
andrewnags <anaga042@uottawa.ca>
2020-06-14 22:14:15 -0400

    new: dev: tests for keep_default decorator

--- tests/unit/decorators_test.py
+++ tests/unit/decorators_test.py
@@ -3,6 +3,7 @@ import pytest
 from pydeepmerge import deep_merge
 from pydeepmerge.strategies.decorators import (
     recurse,
+    keep_default,
 )
 
 
@@ -27,4 +28,24 @@ def test_recurse(test_input, expected):
     assert deep_merge(
         *test_input,
         merge_strategy=recurse(return_right)
+    ) == expected
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        (
+            (
+                {"a": 0, "b": 0},
+                {"a": 1, "c": 1},
+            ),
+            {"a": 1, "b": 0, "c": 1}
+        ),
+    ]
+)
+def test_keep_default(test_input, expected):
+    assert deep_merge(
+        *test_input,
+        merge_strategy=keep_default(return_right)
     ) == expected
