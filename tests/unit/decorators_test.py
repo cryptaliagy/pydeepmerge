d08def45ee675b3efaed051d9245324a33a0822b
andrewnags <anaga042@uottawa.ca>
2020-06-14 22:14:44 -0400

    new: dev: tests for disallow_mixed decorator

--- tests/unit/decorators_test.py
+++ tests/unit/decorators_test.py
@@ -4,6 +4,7 @@ from pydeepmerge import deep_merge
 from pydeepmerge.strategies.decorators import (
     recurse,
     keep_default,
+    disallow_mixed,
 )
 
 
@@ -49,3 +50,55 @@ def test_keep_default(test_input, expected):
         *test_input,
         merge_strategy=keep_default(return_right)
     ) == expected
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "raise_,test_input,expected",
+    [
+        (
+            False,
+            (
+                {"a": 0},
+                {"a": 1},
+            ),
+            {"a": 1}
+        ),
+        (
+            False,
+            (
+                {"a": {"a": 0}},
+                {"a": {"b": 1}},
+            ),
+            {"a": {"b": 1}}
+        ),
+        (
+            True,
+            (
+                {"a": {"a": 0}},
+                {"a": 1},
+            ),
+            {"a": 1}
+        ),
+        (
+            True,
+            (
+                {"a": 0},
+                {"a": {"a": 1}},
+            ),
+            {"a": 1}
+        ),
+    ]
+)
+def test_disallow_mixed(raise_, test_input, expected):
+    if raise_:
+        with pytest.raises(TypeError):
+            deep_merge(
+                *test_input,
+                merge_strategy=disallow_mixed(return_right)
+            )
+    else:
+        assert deep_merge(
+            *test_input,
+            merge_strategy=disallow_mixed(return_right)
+        ) == expected
